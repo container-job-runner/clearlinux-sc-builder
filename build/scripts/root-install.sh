f1d1edfac3dd3b638dbef335a0487d6c69159c12
buvoli <buvoli@uw.edu>
2020-06-08 11:21:44 +0000

    julia tar.gz is now removed after decompression

--- build/scripts/root-install.sh
+++ build/scripts/root-install.sh
@@ -129,6 +129,7 @@ if [ "$LANG_JULIA" = "TRUE" ] ; then
   wget https://julialang-s3.julialang.org/bin/linux/x64/1.4/julia-1.4.2-linux-x86_64.tar.gz
   tar -xzf julia-1.4.2-linux-x86_64.tar.gz
   ln -s /opt/julia-1.4.2/bin/julia /usr/local/bin/julia
+  rm julia-1.4.2-linux-x86_64.tar.gz
 fi
 
 # -----> Jupyter
