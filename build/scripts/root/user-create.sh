fbb494204458195be65ecf28f4b34554453d498a
buvoli <buvoli@uw.edu>
2020-06-09 01:32:29 +0000

    separated root password from user password, removed sudo package install, and allowed empty user password

--- build/scripts/root/user-create.sh
+++ build/scripts/root/user-create.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
-# Change Root Password - or podman cannot execute USER root after USER <user>
-passwd root << EOF
-$USER_PASSWORD
-$USER_PASSWORD
-EOF
-
-# -- Install Sudo Package ------------------------------------------------------
-swupd bundle-add sudo
+# Change Root Password or Docker/Podman cannot execute USER root after USER $USER_NAME
+echo -e "$ROOT_PASSWORD\n$ROOT_PASSWORD" | passwd root
 
 # -- Add User-------------------------------------------------------------------
 if [ -z "$USER_ID" ] || [ -z "$GROUP_ID" ] ; then
-  useradd -m -l -s /bin/bash $USER_NAME
+    useradd -m -l -s /bin/bash $USER_NAME
 else
-  groupadd -o --gid $GROUP_ID $USER_NAME
-  useradd -m -l -o -s /bin/bash --uid $USER_ID --gid $GROUP_ID $USER_NAME
-  passwd $USER_NAME << EOL
-$USER_PASSWORD
-$USER_PASSWORD
-EOL
+    groupadd -o --gid $GROUP_ID $USER_NAME
+    useradd -m -l -o -s /bin/bash --uid $USER_ID --gid $GROUP_ID $USER_NAME
+    if [ -n "$USER_PASSWORD" ] ; then
+        echo -e "$USER_PASSWORD\n$USER_PASSWORD" | passwd $USER_NAME
+    fi    
 fi
 
 # -- Grant sudo ----------------------------------------------------------------
 if [ "$GRANT_SUDO" = "TRUE" ] ; then
-  (usermod -aG wheel $USER_NAME)
+    if [ -n "$USER_PASSWORD" ] ; then
+        (usermod -aG wheel $USER_NAME)
+    else
+        (usermod -aG wheelnopw $USER_NAME)
+    fi
 fi
 
 # -- add user to shared group --------------------------------------------------
