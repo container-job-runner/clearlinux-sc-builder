5e1d507cebc47993cca6e530c119771db8c4b305
buvoli <buvoli@uw.edu>
2020-06-14 05:27:40 +0000

    script now mods user only if ids do not match desired values

--- build/scripts/root/usermod.sh
+++ build/scripts/root/usermod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+  
 # ==============================================================================
 # This script looks for the environment variables 
 #   $DYNAMIC_USER  - user that should be dynamicall modified
@@ -11,9 +11,28 @@
 # NOTE: this script should be run by root
 # ==============================================================================
 
-if [ -n "$DYNAMIC_USER" ] && [ -n "$DYNAMIC_UID" ] && [ -n "$DYNAMIC_GID" ] ; then
+USER_CHANGE="FALSE"
+
+# -- modify user group id ------------------------------------------------------
+if [ -n "$DYNAMIC_USER" ] && [ -n "$DYNAMIC_GID" ] && [ "$(id -g)" != "$DYNAMIC_GID" ] ; then
     groupmod -g $DYNAMIC_GID $DYNAMIC_USER
+    USER_CHANGE="TRUE"
+fi
+
+# -- modify user id ------------------------------------------------------------
+if [ -n "$DYNAMIC_USER" ] && [ -n "$DYNAMIC_UID" ] && [ "$(id -u)" != "$DYNAMIC_UID" ] ; then
     usermod -u $DYNAMIC_UID -g $DYNAMIC_GID $DYNAMIC_USER
-    chown $DYNAMIC_UID:$DYNAMIC_GID /home/$DYNAMIC_USER # note: avoid recusive chown and add any special directories manually. (this will be faster, and we prevent attempting to chown files in bound volumes)
+    USER_CHANGE="TRUE"
+fi
+
+# -- chown files ---------------------------------------------------------------
+# note: avoid recusive chown. Instead add any special directories manually since
+# this will be faster, and it prevents chowning files in bound volumes
+if [ "$USER_CHANGE" = "TRUE" ] && [ -n "$DYNAMIC_UID" ] && [ -n "$DYNAMIC_GID" ] ; then
+    chown $DYNAMIC_UID:$DYNAMIC_GID /home/$DYNAMIC_USER
+fi
+
+# -- start new shell for user --------------------------------------------------
+if [ -n "$DYNAMIC_USER" ] ; then
     exec sudo -E -u $DYNAMIC_USER bash -l -c "$@"
 fi
