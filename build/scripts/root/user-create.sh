786ee4f203be886131db6284cd8d0e470ad6bcd4
buvoli <buvoli@uw.edu>
2020-08-01 03:00:26 +0000

    Refactored password logic in root/user-create.sh and added description.

--- build/scripts/root/user-create.sh
+++ build/scripts/root/user-create.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# -- USER CREATE SCRIPT -------------------------------------------------------
+# Adds a non root user and enables sudo permissions for entrypoint usermod 
+# script. The script responds to the following environmental variables:
+#
+#     USER_ID         ID for new linux user
+#     GROUP_ID        Group ID for new linux user
+#     USER_NAME       username for new linux user
+#     USER_PASSWORD   password for new linux user
+#     GRANT_SUDO      if "TRUE" then new linux user will have sudo privilages
+#     EMPTYHOME       if "TRUE" then the user will be added to group "shared"
+#     DYNAMIC_USER    if "TRUE" then passwordless execution is enabled for 
+#                     usermod script "/opt/build-scripts/usermod.sh"
+# ------------------------------------------------------------------------------
+
 # Change Root Password or Docker/Podman cannot execute USER root after USER $USER_NAME
 echo -e "$ROOT_PASSWORD\n$ROOT_PASSWORD" | passwd root
 
@@ -8,11 +22,13 @@ if [ -z "$USER_ID" ] || [ -z "$GROUP_ID" ] ; then
     useradd -m -l -s /bin/bash $USER_NAME
 else
     groupadd -o --gid $GROUP_ID $USER_NAME
-    useradd -m -l -o -s /bin/bash --uid $USER_ID --gid $GROUP_ID $USER_NAME
-    if [ -n "$USER_PASSWORD" ] ; then
-        echo -e "$USER_PASSWORD\n$USER_PASSWORD" | passwd $USER_NAME
-    fi    
+    useradd -m -l -o -s /bin/bash --uid $USER_ID --gid $GROUP_ID $USER_NAME  
 fi
+
+# -- Set User Password ---------------------------------------------------------
+if [ -n "$USER_PASSWORD" ] ; then
+    echo -e "$USER_PASSWORD\n$USER_PASSWORD" | passwd $USER_NAME
+fi 
 
 # -- Grant sudo ----------------------------------------------------------------
 if [ "$GRANT_SUDO" = "TRUE" ] ; then
